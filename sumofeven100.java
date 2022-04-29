0dffbfe5aae8ee0cbf3fcccf5101f3a9a165916b
csachin42 <csachin42@gmail.com>
2022-04-29 21:11:06 +0530

    Sum of Even Number added

---
+++ sumofeven100.java
@@ -0,0 +1,17 @@
+public class sumofeven100 {
+    public static void main(String[] args) {
+        int i;
+        int sum=0;
+        for (i=0; i<=100;i++){
+            if(i%2==0)
+               sum=sum+i;
+            }
+            System.out.println(sum);
+
+
+        }
+
+
+    }
+
+
